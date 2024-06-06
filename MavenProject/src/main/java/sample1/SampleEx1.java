018fe9dbc86ac2a7a4502673c16fdd1aa099090e
MyPc <MyPc@DESKTOP-UMRLRJO>
2024-06-06 07:28:59 +0530

    Sample commit

---
+++ MavenProject/src/main/java/sample1/SampleEx1.java
@@ -0,0 +1,17 @@
+package sample1;
+
+public class SampleEx1 {
+	public void add(int a,int b)
+	{
+	int c= a+b;
+	System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		SampleEx1 object= new SampleEx1();
+				object.add(20,30);
+		// TODO Auto-generated method stub
+
+	}
+
+}
